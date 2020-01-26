85a697ca17f7352c2b49f852302d2ed21042e39c
Peter Marshall <pwmarshall@gmail.com>
2020-01-25 22:53:44 -0600

    opps

--- login/login.php
+++ login/login.php
@@ -14,10 +14,12 @@
       <form>
         <div id = "labels">
           <label>Username: </label>
+          <br>
           <label>Password: </label>
         </div>
         <div id = "inputs">
           <input type = "text" name = "username"/>
+          <br>
           <input type = "password" name = "password"/>
         </div>
         <input type = "submit"/>
