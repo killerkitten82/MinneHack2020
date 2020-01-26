031efb9848a18a300bb5ad4fd01550a8c85460fc
Peter Marshall <pwmarshall@gmail.com>
2020-01-25 22:39:33 -0600

    first test login

---
+++ login/login.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+  <title>Login Page</title>
+  <link rel="stylesheet" type="text/css" href="login.css">
+</head>
+
+<body>
+  <div id = "LoginBox">
+    <div id = "header">
+      <b>Login Page</b>
+    </div>
+    <div id = "form">
+      <form>
+        <label>Username: </label><input type = "text" name = "username"/>
+        <br>
+        <label>Password: </label><input type = "password" name = "password"/>
+        <br>
+        <input type = "submit">Submit</input>
+      </form>
+    </div>
+  </div>
+</body>
